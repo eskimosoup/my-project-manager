1bec6d4bcadbf3728f3642bd6cb290edab22e1bb
Optimised server <support@eskimosoup.co.uk>
2016-01-13 13:25:41 +0000

    Pagination, more styling, filters

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,7 @@ class CustomersController < ApplicationController
 
   def index
     @customers = Customer.filter(params.slice(:name_search, :project_type)).page(params[:page]).per(params[:per_page] || 1)
+    @presented_customers = collection_presenter(@customers)
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,18 @@ module ApplicationHelper
   def format_area(area)
     "#{ area }m<sup>2</sup>".html_safe
   end
-  
+
   def octicon(code)
     content_tag :span, '', class: "octicon octicon-#{code.to_s.dasherize}"
+  end
+
+  def presenter(object, klass = nil)
+    klass ||= "#{object.class}Presenter".constantize if object.present?
+    klass.new(object: object, view_template: view_context)
+  end
+
+  def collection_presenter(object, klass = nil)
+    klass ||= "#{object.first.class}Presenter".constantize if object.present?
+    BaseCollectionPresenter.new(collection: object, view_template: view_context, presenter: klass)
   end
 end
