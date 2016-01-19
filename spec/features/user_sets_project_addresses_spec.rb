6fcc777abeb64af92ac4e476dacf15176ef2990f
James Doyley <jdoyley@gmail.com>
2016-01-19 10:28:26 +0000

    Add shipping and billing address to projects

--- spec/features/user_sets_project_addresses_spec.rb
+++ spec/features/user_sets_project_addresses_spec.rb
@@ -15,7 +15,7 @@ feature "User sets project addresses" do
     end
   end
 
-  scenario "Changing shipping address" do
+  scenario "Changing shipping address", js: true do
     customer = create(:customer)
     address = create(:address, customer: customer)
     project = create(:project, customer: customer, shipping_address: address)
@@ -34,12 +34,45 @@ feature "User sets project addresses" do
   end
 
   describe "Adding billing address" do
-    scenario "existing address" do
+    scenario "existing address", js: true do
+      project = create(:project)
+      address = create(:address, customer: project.customer)
+
+      visit project_path(project)
+      click_on "Set Billing Address"
+      set_billing_address(address)
+      click_on "Set Billing Address"
+
+      expect(page).to have_content address.line_1
     end
+  end
+
+  scenario "Changing billing address", js: true do
+    customer = create(:customer)
+    address = create(:address, customer: customer)
+    project = create(:project, customer: customer, billing_address: address)
+    second_address = create(:address, customer: customer)
+
+    visit project_path(project)
+    within "#billing-address-details" do
+      click_on "Change"
+    end
+    within "#billing-address-#{ second_address.id }" do
+      click_on "Use"
+    end
+    click_on "Set Billing Address"
+
+    expect(page).to have_content second_address.line_1
   end
 
   def set_shipping_address(address)
     within "#shipping-address-#{ address.id }" do
+      click_on "Use"
+    end
+  end
+
+  def set_billing_address(address)
+    within "#billing-address-#{ address.id }" do
       click_on "Use"
     end
   end
