3997e9f0204ca48fff155905a1329407629935d6
James Doyley <jdoyley@gmail.com>
2016-02-15 14:07:03 +0000

    Update mailer to email addresses

--- app/mailers/envisage_mailer.rb
+++ app/mailers/envisage_mailer.rb
@@ -7,16 +7,18 @@ class EnvisageMailer < ApplicationMailer
   #
   def project_confirmed(project)
     @project = project
+    brand = project.brand
     @billing_address = project.billing_address
     @shipping_address = project.shipping_address
 
-    mail to: "to@example.org"
+    mail to: brand.email, subject: "A project has been confirmed"
   end
 
   def project_finalised(project)
     @project = project
+    brand = project.brand
     envisage_job_sheet = EnvisageJobSheet.new(@project)
     attachments[envisage_job_sheet.filename] = File.read(envisage_job_sheet.to_pdf)
-    mail to: "james@optimised.today"
+    mail to: brand.email, subject: "A project has been finalised"
   end
 end
