2be619b7920d614c400a67ca189f37abce1eb466
Paul <pauloptimised@users.noreply.github.com>
2016-04-20 10:33:44 +0100

    PDF mailer

---
+++ app/mailers/percentage_invoice_mailer.rb
@@ -0,0 +1,20 @@
+class PercentageInvoiceMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.my_mailer.project_finalised.subject
+  #
+  def invoice_created(project, invoice)
+    @project = project
+    @invoice = invoice
+    brand = project.brand
+    invoice_pdf = InvoiceDownload.new(@invoice)
+    attachments[invoice_pdf.filename] = File.read(invoice_pdf.to_pdf)
+    if Rails.env.production?
+      mail to: 'purchasing@ludostudio.co.uk', subject: "Invoice #{@invoice.number} has been created for #{brand.name}"
+    else
+      mail to: 'support@optimised.today', subject: "Invoice #{@invoice.number} has been created for #{brand.name}"
+    end
+  end
+end
