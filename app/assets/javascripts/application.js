02dc6b5e4b7738915ecd07eee890e5f369738809
Paul <pauloptimised@users.noreply.github.com>
2016-04-21 15:56:43 +0100

    Calculator as global include, due to turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,6 +55,7 @@
 // require jquery-ui/effect-transfer
 //= require components/sticky-internal-navigation
 //= require components/tooltip
+//= require components/area-calculator
 // require_tree .
 
 $(function() {
