e745e1434a64537d9f28a74c8a1542787e7cca37
James Doyley <jdoyley@gmail.com>
2016-05-13 17:13:09 +0100

    Update status changed finalised

--- app/models/status_changer/finalised.rb
+++ app/models/status_changer/finalised.rb
@@ -13,7 +13,7 @@ module StatusChanger
     end
 
     def project_attributes_to_reject
-      %w(id created_at updated_at customer_id brand_id contact_id status shipping_address_id billing_address_id)
+      %w(id created_at updated_at customer_id brand_id contact_id status shipping_address_id billing_address_id finalised_at completed_at)
     end
 
     def initialize(project, attributes = default_attributes(project))
