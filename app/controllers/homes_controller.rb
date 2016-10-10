e094617f99ddbd985c8e572b0162302102b12062
optimised <noreply@optimised.today>
2016-10-10 10:46:08 +0100

    Finances link

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,9 @@
 class HomesController < ApplicationController
-  skip_before_action :require_login
+  skip_before_action :require_login, only: :show
 
   def show
+  end
+
+  def finances
   end
 end
