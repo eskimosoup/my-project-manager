e6b0708294a88082148ab601e196bf3e73d72d97
optimised <noreply@optimised.today>
2016-10-11 09:21:16 +0100

    Envisage invoices

--- app/models/invoice_download.rb
+++ app/models/invoice_download.rb
@@ -7,7 +7,7 @@ class InvoiceDownload
     @invoice = invoice
     @envisage_invoice = envisage_invoice
     @project = invoice.project
-    @colour = project.colour
+    @colour = (envisage_invoice == false ? project.colour : '#56331a')
   end
 
   def to_pdf
