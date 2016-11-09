54ca8fc0c4214c7a3458dc712321b4c80466e3a6
optimised <noreply@optimised.today>
2016-11-09 15:15:05 +0000

    Add new brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,19 +1,27 @@
 class Brand < ActiveRecord::Base
-  MY_BRANDS = %w( my_office_branding my_vehicle_wrap my_print_services ).freeze 
+  MY_BRANDS = %w( my_office_branding my_vehicle_wrap my_print_services my_design my_internet_marketing ).freeze
 
-  scope :my_brand, ->{ where(brand_type: Brand.brand_types.select{|x| Brand::MY_BRANDS.include?(x) }.values) }
-  scope :envisage_brand, ->{ where.not(brand_type: Brand.brand_types.select{|x| Brand::MY_BRANDS.include?(x) }.values) }
+  scope :my_brand, -> { where(brand_type: Brand.brand_types.select { |x| Brand::MY_BRANDS.include?(x) }.values) }
+  scope :envisage_brand, -> { where.not(brand_type: Brand.brand_types.select { |x| Brand::MY_BRANDS.include?(x) }.values) }
 
   has_one :brand_address
 
-  enum brand_type: [:my_office_branding, :envisage, :envisage_trade, :my_vehicle_wrap, :my_print_services]
+  enum brand_type: [
+    :my_office_branding,
+    :envisage,
+    :envisage_trade,
+    :my_vehicle_wrap,
+    :my_print_services,
+    :my_design,
+    :my_internet_marketing
+  ]
   validates :name, presence: true
   validates :email, presence: true
   validates :brand_type, presence: true
   validates :account_management_rate, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
 
   delegate :logo, :colour, to: :brand_graphics
-  
+
   def brand_graphics
     @brand_graphics ||= BrandGraphics.klass_for(brand_type).new
   end
@@ -23,18 +31,18 @@ class Brand < ActiveRecord::Base
   end
 
   def my_print_services?
-    brand_type == "my_print_services"
+    brand_type == 'my_print_services'
   end
 
   def vehicle_brand?
-    brand_type == "my_vehicle_wrap"
+    brand_type == 'my_vehicle_wrap'
   end
 
   def prefix
     if my_brand?
-      "MY"
+      'MY'
     else
-      "EN"
+      'EN'
     end
   end
 end
---
+++ app/models/my_design_brand_graphics.rb
@@ -0,0 +1,9 @@
+class MyDesignBrandGraphics < BrandGraphics
+  def logo
+    'logos/my-design.jpg'
+  end
+
+  def colour
+    '#1cdbc8'
+  end
+end
