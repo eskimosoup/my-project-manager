195c3d73c58fe62a960b14867c86d83ec31459f4
optimised <noreply@optimised.today>
2017-01-11 16:05:50 +0000

    Amend mark up specifically for projects > id

--- app/models/price_calculator/sundry_item.rb
+++ app/models/price_calculator/sundry_item.rb
@@ -2,7 +2,7 @@ class PriceCalculator::SundryItem
   include PricingDefaults
 
   attr_reader :sundry_item
-  delegate :cost, to: :sundry_item
+  delegate :cost, :print_job, to: :sundry_item
 
   def initialize(sundry_item:)
     @sundry_item = sundry_item
@@ -11,14 +11,14 @@ class PriceCalculator::SundryItem
   def envisage_price
     envisage_mark_up * cost
   end
-  alias_method :envisage_trade_price, :envisage_price
-  alias_method :envisage_to_my_price, :envisage_price
+  alias envisage_trade_price envisage_price
+  alias envisage_to_my_price envisage_price
 
   def envisage_rush_price
     rush_job_mark_up * envisage_price
   end
-  alias_method :envisage_trade_rush_price, :envisage_rush_price
-  alias_method :envisage_to_my_rush_price, :envisage_rush_price
+  alias envisage_trade_rush_price envisage_rush_price
+  alias envisage_to_my_rush_price envisage_rush_price
 
   def my_price
     (my_mark_up * envisage_to_my_price).round(2)
@@ -31,6 +31,18 @@ class PriceCalculator::SundryItem
   private
 
   def my_mark_up
-    BigDecimal("1.16666666667")
+    if sundry_item.print_job.project.id > 1145 || sundry_item.print_job.project.id == 1077
+      BigDecimal('1.2')
+    else
+      BigDecimal('1.16666666667')
+    end
+  end
+
+  def envisage_mark_up
+    if sundry_item.print_job.project.created_at > 1145 || sundry_item.print_job.project.id == 1077
+      BigDecimal('1')
+    else
+      BigDecimal('1.2')
+    end
   end
 end
