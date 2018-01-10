ecbe5cb7d9f9ea07b914197bcb14610f274523b6
optimised <noreply@optimised.today>
2018-01-10 12:29:46 +0000

    Remove base invoice number

--- app/dashboards/company_dashboard.rb
+++ app/dashboards/company_dashboard.rb
@@ -41,7 +41,7 @@ class CompanyDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :base_invoice_number,
+    # :base_invoice_number,
     :name,
   ].freeze
 
